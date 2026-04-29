e0ab39d6901dac231e95f2c58a54d5530f8058d5
muhammedfayazp <muhammedfayaz@yahoo.in>
2026-04-29 19:49:56 +0400

    menu

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,3 +1,3 @@
 export default function Features(){
-    return <div>Features</div>
+    return <div> </div>
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,9 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+
 export default function Navbar(){
-    return <div className="fixed top-0 w-full z-50 transition-all duration-300 bg-red-950/20 backdrop-blur-sm">
+    const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false);
+    return <nav className="fixed top-0 w-full z-50 transition-all duration-300 bg-slate-950/20 backdrop-blur-sm">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between item-center h-14 sm:h-16 md:h-20">
                 <div className="flex items-center space-x-1 group cursor-pointer">
@@ -13,8 +17,26 @@ export default function Navbar(){
                 </div>
 
                 {/* Navlinks */}
-                <div className="hidden"></div>
+                <div className="hidden md:flex items-center space-x-6 lg:space-x-8">
+                    <a href="#features" className="text-gray-300 hover:text-300 text-sm lg:text-base">Features</a>
+                    <a href="#pricing" className="text-gray-300 hover:text-300 text-sm lg:text-base">Pricing</a>
+                    <a href="#testimonials" className="text-gray-300 hover:text-300 text-sm lg:text-base">Testimonials</a>
+                </div>
+                <button className="md:hidden p-2 text-gray-300 hover:text-white" onClick={()=>setMobileMenuIsOpen((prev)=>!prev)}>
+                    {mobileMenuIsOpen?(
+                        <X className="w-5 h-5 sm:w-6 sm:h-6"/>                        
+                    ):(
+                        <Menu className="w-5 h-5 sm:w-6 sm:h-6"/>
+                    )}
+                </button>
             </div>
         </div>
-    </div>;
+        {mobileMenuIsOpen && <div>
+            <div className="md:hidden">
+                <a href="#features" className="text-gray-300 hover:text-300 text-sm lg:text-base">Features</a>
+                <a href="#pricing" className="text-gray-300 hover:text-300 text-sm lg:text-base">Pricing</a>
+                <a href="#testimonials" className="text-gray-300 hover:text-300 text-sm lg:text-base">Testimonials</a>
+            </div>
+        </div>}
+    </nav>;
 }
